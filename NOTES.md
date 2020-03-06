40574610b554570a3cf13f6f60b33f1da0b910a4
Steven Mey <mail@steven-mey.de>
2020-03-06 21:14:10 +0100

    Created a blacklist and add calculator

---
+++ NOTES.md
@@ -0,0 +1,5 @@
+# Documentation
+https://gjs-docs.gnome.org/
+
+Maximize-Flags:
+https://gjs-docs.gnome.org/meta5~5_api/meta.maximizeflags
--- extension.js
+++ extension.js
@@ -4,24 +4,31 @@ const ExtensionUtils = imports.misc.extensionUtils;
 let _windowCreatedId;
 let _settings;
 
+
 function init() {
     _settings = ExtensionUtils.getSettings();
 }
+
+var blacklist = ['Calculator'];
+
 function enable() {
     _windowCreatedId = global.display.connect('window-created', (d, win) => {
-        if (win.can_maximize()) {
-            if (_settings.get_boolean('vertical')) {
-                win.maximize(Meta.MaximizeFlags.VERTICAL)
+        if (! blacklist.includes(win.title)) {
+            if (win.can_maximize()) {
+                if (_settings.get_boolean('vertical')) {
+                    win.maximize(Meta.MaximizeFlags.VERTICAL);
+                }
+                if (_settings.get_boolean('horizontal')) {
+                    win.maximize(Meta.MaximizeFlags.HORIZONTAL);
+                }
             }
-            if (_settings.get_boolean('horizontal')) {
-                win.maximize(Meta.MaximizeFlags.HORIZONTAL)
-            }
-            
         }
+        // output in: journalctl /usr/bin/gnome-shell -f
+        // global.log('gnome-extension maxi@darkretailer.github.com: "' + win.title + '"');
     });
 }
 
 function disable() {
     global.display.disconnect(_windowCreatedId);
-    _setting = null;
+    // _setting = null;
 } 
