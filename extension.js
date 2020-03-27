08c9090aeb3103fdd688ef259e3be96bbfcf913b
Steven Mey <darkretailer@users.noreply.github.com>
2020-03-28 00:30:36 +0100

    add debug-var

--- extension.js
+++ extension.js
@@ -4,6 +4,7 @@ var ExtensionUtils = imports.misc.extensionUtils;
 var _windowCreatedId;
 var _settings;
 
+var debug = false;
 
 function init() {
     _settings = ExtensionUtils.getSettings();
@@ -11,6 +12,10 @@ function init() {
 
 function enable() {
     _windowCreatedId = global.display.connect('window-created', (d, win) => {
+        if (debug) {
+            global.log("gnome-extension maxi@darkretailer.github.com: New " + win.gtk_application_id);
+        }
+        
         if (! this._settings.get_strv('blacklisted-apps').includes(win.gtk_application_id + ".desktop")) {
             if (win.can_maximize()) {
                 if (_settings.get_boolean('vertical')) {
@@ -21,7 +26,9 @@ function enable() {
                 }
             }
         } else {
-            global.log('gnome-extension maxi@darkretailer.github.com: "' + win.gtk_application_id + '" is blacklisted');
+            if (debug) {
+                global.log('gnome-extension maxi@darkretailer.github.com: "' + win.gtk_application_id + '" is blacklisted');
+            }
         }
     });
 }
